bc99f581a06e24ab71fe5af7fac718ab96b9e2bf
Paola <paolacaicedouis@gmail.com>
2017-12-16 17:15:08 -0500

    act

--- htmls/encabezado.php
+++ htmls/encabezado.php
@@ -40,11 +40,10 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Repair Maxter Cell</a>
+      <a class="navbar-brand" href="./home.php">Repair Maxter Cell</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="./home.php">Home</a></li>
       <li class="dropdown"><a href="#">Page 1</a></li>
       <li class="dropdown"><a href="#">Page 2</a></li>
       <li class="dropdown"><a href="./ubication.php">Nuestra ubicación</a></li>
--- htmls/ubication.php
+++ htmls/ubication.php
@@ -23,19 +23,24 @@
     <div class="col-sm-2"></div>
     <div class="col-sm-8" style="background-color:#dddddd;">
     	<!-- <h3>Aquí estamos: </h3> -->
+    	<br>
+    	<br>
     	<div class="titulo" style="padding-top:20px;color: #000057;" align="center"> Nuestra ubicación</div>
     	<p style="font-size:1.2em;color:#000023; margin:20px;"> 
-    		Si te encuentras interezado en alguno de nuestros productos, visitanos en nuestra tienda fisica y con gusto te atenderemos.
+    		Si te encuentras interesado en alguno de nuestros productos, visitanos en nuestra tienda fisica y con gusto te atenderemos.
     		<br>
     	</p>
     	<p align="center">
     		Nuestra dirección: "No me la sé, toca que se la pregunten al men :v" <br>
     	</p>
+    	<br>
     	 <!-- En este div se proyecta el mapa -->
     	<div id="map"></div>
 
     	<center>
     		<a target="_blank" href="https://www.google.com.co/maps/place/7%C2%B008'11.6%22N+73%C2%B007'27.5%22W/@7.1365533,-73.1264797,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d7.136548!4d-73.124291"> <button type="button" class="btn btn-primary">Ver en Google Maps</button> </a>
+    		<br>
+    		<br>
     		<br>
     		<br>
     	</center>
