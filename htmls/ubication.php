5b2e15282d0f3c33d72ca6a786a3ecd228824436
Paola <paolacaicedouis@gmail.com>
2017-12-16 14:54:46 -0500

    act

--- htmls/ubication.php
+++ htmls/ubication.php
@@ -2,20 +2,31 @@
 <?php 
 // AIzaSyBCDrwD0qSoTb7e96i2R_KH0_fSxTSs6hQ API KEY
  include("./encabezado.php");
+ 
  ?>
+
  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCDrwD0qSoTb7e96i2R_KH0_fSxTSs6hQ&callback=initMap"
   type="text/javascript"></script>
   <head>
     <style>
+       /*Dimenciones del mapa*/
        #map {
-        height: 400px;
+        height: 400px; 
         width: 100%;
        }
     </style>
-  </head>
-  <body>
-    <h3>Aquí estamos: </h3>
-    <div id="map" style="border-radius:10px;"></div> <?php //En este div se proyecta el mapa ?>
+<div class="container-fluid">
+<div class="row">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8" style="background-color:#dddddd;">
+    	<h3>Aquí estamos: </h3>
+    	<div id="map" style="border-radius:10px;margin-bottom:10px;"></div> <?php //En este div se proyecta el mapa ?>
+    	<a target="_blank" href="https://www.google.com.co/maps/place/7%C2%B008'11.6%22N+73%C2%B007'27.5%22W/@7.1365533,-73.1264797,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d7.136548!4d-73.124291"> Haz click aquí para verlo directamente en Google maps. </a>
+    </div>
+</div>
+</div>
+</div>
+    
 
     <script>
       function initMap() {
@@ -33,5 +44,5 @@
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCDrwD0qSoTb7e96i2R_KH0_fSxTSs6hQ&callback=initMap">
     </script>
-    <a target="_blank" href="https://www.google.com.co/maps/place/7%C2%B008'11.6%22N+73%C2%B007'27.5%22W/@7.1365533,-73.1264797,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d7.136548!4d-73.124291"> Haz click aquí para verlo directamente en Google maps. </a>
+    
   </body>
