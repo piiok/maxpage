3f1439b71054a82d1eeab8fcfb7c46d3f10c2314
Paola <paolacaicedouis@gmail.com>
2017-12-16 15:45:10 -0500

    act

--- htmls/ubication.php
+++ htmls/ubication.php
@@ -20,13 +20,17 @@
     <div class="col-sm-2"></div>
     <div class="col-sm-8" style="background-color:#dddddd;">
     	<h3>Aquí estamos: </h3>
+
     	<div id="map" style="border-radius:10px;margin-bottom:10px;"></div> <?php //En este div se proyecta el mapa ?>
-    	<a target="_blank" href="https://www.google.com.co/maps/place/7%C2%B008'11.6%22N+73%C2%B007'27.5%22W/@7.1365533,-73.1264797,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d7.136548!4d-73.124291"> Haz click aquí para verlo directamente en Google maps. </a>
+
+    	<center>
+    		<a target="_blank" href="https://www.google.com.co/maps/place/7%C2%B008'11.6%22N+73%C2%B007'27.5%22W/@7.1365533,-73.1264797,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d7.136548!4d-73.124291"> <button type="button" class="btn btn-primary">Ver en Google Maps</button> </a>
+    		<br>
+    		<br>
+    	</center>
     </div>
 </div>
 </div>
-</div>
-    
 
     <script>
       function initMap() {
