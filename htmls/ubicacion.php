fa3391bca56f3325d284d5183e70aea9e2ea6883
eduardcaballero <eduardcaballerop@gmail.com>
2017-12-16 17:25:46 -0500

    act
    nuevo mapa

---
+++ htmls/ubicacion.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Mapa maxtercell</title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script>
+
+      function initMap() {
+        var myLatLng = {lat: 7.136705058020035, lng: -73.12423657625914};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: myLatLng
+        });
+
+        var image = '../img/icono.png';
+        var beachMarker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          icon: image,
+          animation: google.maps.Animation.DROP,
+          title: 'CRA 23 #10-53, San Francisco'
+        });
+
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+
+    </script>
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1FVgmPQqQZdib0l5pQ2xwv2Kv0hHpD6k&callback=initMap">
+    </script>
+  </body>
+</html>
--- htmls/ubication.php
+++ htmls/ubication.php
@@ -1,73 +1,26 @@
 
-<?php 
+<?php
 // AIzaSyBCDrwD0qSoTb7e96i2R_KH0_fSxTSs6hQ API KEY
  include("./encabezado.php");
- 
- ?>
 
- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCDrwD0qSoTb7e96i2R_KH0_fSxTSs6hQ&callback=initMap"
-  type="text/javascript"></script>
-  <head>
-    <style>
-       /*Dimenciones del mapa*/
-       #map {
-        height: 400px; 
-        width: 80%;
-        border-radius:10px;
-        margin-bottom:10px;
-        margin-left:10%;
-        margin-right:10%;
-       }
-    </style>
+ ?>
 <div class="row">
-    <div class="col-sm-2"></div>
-    <div class="col-sm-8" style="background-color:#dddddd;">
     	<!-- <h3>Aquí estamos: </h3> -->
-    	<br>
-    	<br>
-    	<div class="titulo" style="padding-top:20px;color: #000057;" align="center"> Nuestra ubicación</div>
-    	<p style="font-size:1.2em;color:#000023; margin:20px;"> 
-    		Si te encuentras interesado en alguno de nuestros productos, visitanos en nuestra tienda fisica y con gusto te atenderemos.
+    	<div class="titulo" style="padding-top:20px;color: #3B5998;" align="center"> Nuestra ubicación</div>
+      <p style="font-size:1.2em;color:#000023; margin:20px;">
+    		Si te encuentras interesado en alguno de nuestros productos o servicios, visitanos en nuestra tienda fisica y con gusto lo atenderemos.
     		<br>
     	</p>
-    	<p align="center">
-    		Nuestra dirección: "No me la sé, toca que se la pregunten al men :v" <br>
-    	</p>
-    	<br>
+      <iframe src="./ubicacion.php" width="100%" height="400" frameborder="0"></iframe>
+
     	 <!-- En este div se proyecta el mapa -->
-    	<div id="map"></div>
 
     	<center>
-    		<a target="_blank" href="https://www.google.com.co/maps/place/7%C2%B008'11.6%22N+73%C2%B007'27.5%22W/@7.1365533,-73.1264797,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d7.136548!4d-73.124291"> <button type="button" class="btn btn-primary">Ver en Google Maps</button> </a>
-    		<br>
-    		<br>
-    		<br>
-    		<br>
+        <br>
+    		<a target="_blank" href="https://goo.gl/maps/UoyKLavFTh32"> <button type="button" class="btn btn-primary">Ver en Google Maps</button> </a>
+    		<br><br>
     	</center>
-    </div>
 </div>
-
-
-    <script>
-      function initMap() {
-        var uluru = {lat: 7.136548, lng: -73.124291}; // direcciones geograficas de repair maxter cell
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: uluru,
-          zoom:8, 
-          scrollwheel:false, 
-          draggable:false, 
-          mapTypeId:google.maps.MapTypeId.ROADMAP
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCDrwD0qSoTb7e96i2R_KH0_fSxTSs6hQ&callback=initMap">
-    </script>
-    
-  <?php 
-	include("./footer.php");
-   ?>
+<?php
+include("./footer.php");
+?>
