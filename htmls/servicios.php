7b4ad5722e18151f70c126455d50336739bb3aa0
Paola <paolacaicedouis@gmail.com>
2017-12-16 18:52:59 -0500

    act

--- htmls/servicios.php
+++ htmls/servicios.php
@@ -2,11 +2,47 @@
 // AIzaSyBCDrwD0qSoTb7e96i2R_KH0_fSxTSs6hQ API KEY
 include("./encabezado.php");
 ?>
-
-<div class="col-sm-4">
-      <span class="glyphicon glyphicon-wrench logo-small"></span>
-      <h4 style="color:#303030;">SERVICIO TECNICO</h4>
-      <p>Se arregla todo tipo de aparato tecnologico descompuesto</p>
+<style type="text/css">
+.logo-small {
+      color: #f4511e;
+      font-size: 100px;
+  }
+  h2{
+  	font-size: 4em;
+  	margin-bottom: 40px;
+  }
+h4 {
+      font-size: 19px;
+      line-height: 1.375em;
+      color: #303030;
+      font-weight: 400;
+      margin-bottom: 30px;
+      margin-top: 30px;
+  }
+  .slide{
+  	padding-top: 3%;
+  	padding-bottom: 15%;
+  	background-color: 
+  }
+</style>
+<br>
+<div class="slide text-center" high="100%">
+	<a id="servicios"></a>
+	<h2>SERVICIOS</h2>
+	<div class="row slideanim">
+		<div class="col-sm-2">
+		</div>	
+		<div class="col-sm-4">
+		      <span class="glyphicon glyphicon-shopping-cart logo-small"></span>
+		      <h4 style="color:#303030;">VENTA DE ARTICULOS TECNOLÓGICOS</h4>
+		      <!-- <p>Venta de accesorios y artículos tecnológicos nuevos y usados.</p> -->
+		</div>
+		<div class="col-sm-4">
+		      <span class="glyphicon glyphicon-wrench logo-small"></span>
+		      <h4 style="color:#303030;">SERVICIO TÉCNICO</h4>
+		      <!-- <p>Soporte técnico para aparatos tecnológicos.</</p> -->
+		</div>
+	</div>
 </div>
 
 <?php
