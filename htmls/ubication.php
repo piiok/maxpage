c55f02d4ec28415e1813bbc3609e284fca64b336
Paola <paolacaicedouis@gmail.com>
2017-12-16 16:38:52 -0500

    act

--- htmls/ubication.php
+++ htmls/ubication.php
@@ -20,14 +20,9 @@
     <div class="col-sm-8" style="background-color:#dddddd;">
     	<!-- <h3>Aquí estamos: </h3> -->
     	<div class="titulo" style="padding-top:20px;color: #3B5998;" align="center"> Nuestra ubicación</div>
-    	<div class="titulo" style="padding-top:20px;color: #3B5998;" align="center"> Nuestra ubicación</div>
-    	<div class="titulo" style="padding-top:20px;color: #3B5998;" align="center"> Nuestra ubicación</div>
-    	<div class="titulo" style="padding-top:20px;color: #3B5998;" align="center"> Nuestra ubicación</div>
-    	<div class="titulo" style="padding-top:20px;color: #3B5998;" align="center"> Nuestra ubicación</div>
-    	<div class="titulo" style="padding-top:20px;color: #3B5998;" align="center"> Nuestra ubicación</div>
-    	<div class="titulo" style="padding-top:20px;color: #3B5998;" align="center"> Nuestra ubicación</div>
-    	
-    	<div id="map" style="border-radius:10px;margin-bottom:10px;"></div> <?php //En este div se proyecta el mapa ?>
+
+    	 <!-- En este div se proyecta el mapa -->
+    	<div id="map" style="border-radius:10px;margin-bottom:10px;"></div>
 
     	<center>
     		<a target="_blank" href="https://www.google.com.co/maps/place/7%C2%B008'11.6%22N+73%C2%B007'27.5%22W/@7.1365533,-73.1264797,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d7.136548!4d-73.124291"> <button type="button" class="btn btn-primary">Ver en Google Maps</button> </a>
