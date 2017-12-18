bc0cc3c16c226dbcf08e4ec92cba36200be3be80
juanse1080 <juanmarcon1080@gmail.com>
2017-12-18 12:33:17 -0500

    "productos"

---
+++ htmls/recomendados.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(0);
+include("./encabezado.php");
+// AIzaSyBCDrwD0qSoTb7e96i2R_KH0_fSxTSs6hQ API KEY
+ ?>
+ <br><br><br>
+<div class="container text-center">    
+  <h2>Recomendados</h2><br>
+  <div class="row">
+      <div class="col-sm-4 col-md-4">
+        <img src="https://i2.linio.com/p/bc7ab0b73be5f0bee913cd137984e9e9-catalog.jpg " class="img-responsive" style="width:100%" alt="Image">
+        <p>hola</p>
+        <button type="button" class="btn btn-primary btn-block">Button 1</button>
+      </div>
+  </div>
+</div>
